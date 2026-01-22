8873381661b0a42f71d781d6ecbdd5b8604f7274
Giovanni Diego Gutierrez Castelan <240082@utxicotepec.edu.mx>
2026-01-22 13:34:21 -0600

    Primer commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+function App(){
+  return (
+    <div>
+      <h1>5 A EVND :b</h1>
+      <h2>Profesor:</h2>
+      <h3>MTI Ricardo Luna Santos</h3>
+      <h3>Alumnos</h3>
+      <h4>Juan</h4>
+      <h4>Diego</h4>
+      <h4>Manuel</h4>
+      <h4>Pancho</h4>
+    </div>
+  )
+}
+
+export default App
